457f9c8e047d3603a3b4043db379447f66cb0d6b
raychenon <raymond.chenon@gmail.com>
2021-05-07 09:54:31 +0200

    reformatting

--- src/test/java/org/stdg/test/DeleteTest.java
+++ src/test/java/org/stdg/test/DeleteTest.java
@@ -13,28 +13,13 @@
 
 package org.stdg.test;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.stdg.SqlTestDataGenerator;
 
-import javax.sql.DataSource;
-
+import static org.stdg.test.TestTable.TestTableAssert.assertThat;
 import static org.stdg.test.TestTable.buildUniqueTable;
 
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-public class DeleteTest {
-
-    private static DataSource DATA_SOURCE;
-
-    private static SqlExecutor SQL_EXECUTOR;
-
-    @BeforeAll
-    public static void beforeAll() {
-        DATA_SOURCE = DataSourceBuilder.build("jdbc:h2:mem:test", "user", "pwd");
-        SQL_EXECUTOR = new SqlExecutor(DATA_SOURCE);
-    }
+public class DeleteTest extends H2Configuration {
 
     @Test
     public void
@@ -43,28 +28,28 @@ public class DeleteTest {
         // GIVEN
         TestTable table1 =
                 buildUniqueTable(DATA_SOURCE
-                        , "Table_1"
-                        , "  id bigint"
+                                , "Table_1"
+                                , "  id bigint"
                                 + ", Col_A varchar(255)"
                                 + ", Col_B varchar(255)"
                                 + ", Col_dec decimal")
-                        .create()
-                        .insertValues("1, 'A1', 'B1', 1.80")
-                        .insertValues("2, 'A2', 'B2', 2.99");
+                .create()
+                .insertValues("1, 'A1', 'B1', 1.80")
+                .insertValues("2, 'A2', 'B2', 2.99");
+
+        String deleteQuery = "DELETE FROM " + table1.getTableName();
 
         // WHEN
-        String table1Name = table1.getTableName();
-        String deleteQuery = "DELETE FROM " + table1Name;
-
         SqlTestDataGenerator sqlTestDataGenerator = SqlTestDataGenerator.buildFrom(DATA_SOURCE);
         String insertScript = sqlTestDataGenerator.generateInsertScriptFor(deleteQuery);
 
         // THEN
         table1.recreate();
         SQL_EXECUTOR.execute(insertScript);
-        TestTable.TestTableAssert.assertThat(table1).withScript(insertScript).hasNumberOfRows(2);
-        TestTable.TestTableAssert.assertThat(table1).withScript(insertScript).row(0).hasValues(1, "A1", "B1", 1.80);
-        TestTable.TestTableAssert.assertThat(table1).withScript(insertScript).row(1).hasValues(2, "A2", "B2", 2.99);
+        assertThat(table1).withScript(insertScript)
+                          .hasNumberOfRows(2)
+                          .row(0).hasValues(1, "A1", "B1", 1.80)
+                          .row(1).hasValues(2, "A2", "B2", 2.99);
     }
 
     @Test
@@ -74,27 +59,29 @@ public class DeleteTest {
         // GIVEN
         TestTable table1 =
                 buildUniqueTable(DATA_SOURCE
-                        , "Table_1"
-                        , "  id bigint"
+                                , "Table_1"
+                                , "  id bigint"
                                 + ", Col_A varchar(255)"
                                 + ", Col_B varchar(255)"
                                 + ", Col_dec decimal")
-                        .create()
-                        .insertValues("1, 'A1', 'B1', 1.80")
-                        .insertValues("2, 'A2', 'B2', 2.99");
+                .create()
+                .insertValues("1, 'A1', 'B1', 1.80")
+                .insertValues("2, 'A2', 'B2', 2.99");
+
+        String updateQuery = "DELETE FROM " + table1.getTableName()
+                           + " WHERE Col_A = 'A1'";
 
         // WHEN
-        String table1Name = table1.getTableName();
-        String updateQuery = "DELETE FROM " + table1Name
-                + " WHERE Col_A = 'A1'";
-
         SqlTestDataGenerator sqlTestDataGenerator = SqlTestDataGenerator.buildFrom(DATA_SOURCE);
         String insertScript = sqlTestDataGenerator.generateInsertScriptFor(updateQuery);
 
         // THEN
         table1.recreate();
         SQL_EXECUTOR.execute(insertScript);
-        TestTable.TestTableAssert.assertThat(table1).withScript(insertScript).hasNumberOfRows(1);
-        TestTable.TestTableAssert.assertThat(table1).withScript(insertScript).row(0).hasValues(1, "A1", "B1", 1.80);
+        assertThat(table1).withScript(insertScript)
+                          .hasNumberOfRows(1)
+                          .row(0).hasValues(1, "A1", "B1", 1.80);
+
     }
+
 }
