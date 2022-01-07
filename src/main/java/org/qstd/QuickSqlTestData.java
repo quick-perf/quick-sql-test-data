bd17bc11e771a69ab4b77ff283fcd0348370e843
Jean Bisutti <jean.bisutti@gmail.com>
2022-01-07 11:22:43 +0100

    Add Javadoc

--- src/main/java/org/qstd/QuickSqlTestData.java
+++ src/main/java/org/qstd/QuickSqlTestData.java
@@ -51,7 +51,7 @@ public class QuickSqlTestData {
 
     /**
      * Factory method to build an instance of <code>org.qstd.QuickSqlTestData</code> from a data source.
-     * The retrieval of database metadata, such as not null columns or primary keys, is cached for each table.
+     * <em>The retrieval of database metadata, such as not null columns or primary keys, is cached for each table.</em>
      * @param dataSource A data source
      * @return An instance of <code>org.qstd.QuickSqlTestData</code>
      */
@@ -63,25 +63,61 @@ public class QuickSqlTestData {
         return buildFrom(dataSource, dbType, databaseMetadataFinderWithCache);
     }
 
+    /**
+     * Factory method to build an instance of <code>org.qstd.QuickSqlTestData</code> from a data source,
+     * a database type and a database metadata finder.
+     * @param dataSource A datasource
+     * @param dbType A database type
+     * @param databaseMetadataFinder A database metadata finder
+     * @return An instance of <code>org.qstd.QuickSqlTestData</code>
+     */
     public static QuickSqlTestData buildFrom(DataSource dataSource, DatabaseType dbType, DatabaseMetadataFinder databaseMetadataFinder) {
         DatasetRowsGenerator datasetRowsGenerator = new DatasetRowsGenerator(dataSource, dbType, databaseMetadataFinder);
         return new QuickSqlTestData(datasetRowsGenerator, dbType);
     }
 
+    /**
+     * Generates an SQL script allowing to test the SQL query given in parameter.
+     * This script contains INSERT statements.
+     * It takes into account the database integrity constraints.
+     * @param sqlQuery An SQL query
+     * @return An SQL script allowing to test the SQL query given in parameter
+     */
     public String generateInsertScriptFor(String sqlQuery) {
         return generateInsertScriptFor(sqlQuery, emptyList());
     }
 
+    /**
+     * Generates an SQL script allowing to test an SQL query with its bind parameter values.
+     * This script contains INSERT statements.
+     * It takes into account the database integrity constraints.
+     * @param query An SQL query with bind parameters
+     * @param parameters Bind parameter values
+     * @return An SQL script allowing to test the SQL query with  its bind parameter values
+     */
     public String generateInsertScriptFor(String query, List<Object> parameters) {
         List<SqlQuery> sqlQueries = singletonList(new SqlQuery(query, parameters));
         return generateInsertScriptFor(sqlQueries);
     }
 
+    /**
+     * Generates an SQL script allowing to test the list of SQL queries given in parameter.
+     * This script contains INSERT statements.
+     * It takes into account the database integrity constraints.
+     * @param sqlQueries SQL queries
+     * @return An SQL script allowing to test the SQL queries given in parameter
+     */
     public String generateInsertScriptFor(List<SqlQuery> sqlQueries) {
         List<DatasetRow> datasetRows = datasetRowsGenerator.generateDatasetRowsFor(sqlQueries);
         return insertStatementGenerator.generateInsertScriptFor(datasetRows);
     }
 
+    /**
+     * Generates a list of INSERT statements allowing to test the list of SQL queries given in parameter.
+     * These INSERT statements take into account the database integrity constraints.
+     * @param sqlQueries SQL queries
+     * @return An SQL script allowing to test the SQL queries given in parameter
+     */
     public String generateInsertScriptFor(String... sqlQueries) {
         List<SqlQuery> queries = stream(sqlQueries)
                                 .map(SqlQuery::new)
@@ -89,11 +125,23 @@ public class QuickSqlTestData {
         return generateInsertScriptFor(queries);
     }
 
+    /**
+     * Generates a list of INSERT statements allowing to create in database the dataset row given in parameter.
+     * These INSERT statements take into account the database integrity constraints.
+     * @param datasetRow A dataset row
+     * @return A list of INSERT statements allowing to create in database the dataset row given in parameter
+     */
     public List<String> generateInsertListFor(DatasetRow datasetRow) {
         SqlQuery sqlQuery = SqlQuery.buildFromRow(datasetRow, dbType);
         return generateInsertListFor(sqlQuery.toString());
     }
 
+    /**
+     * Generates a list of INSERT statements allowing to test the SQL queries given in parameter.
+     * These INSERT statements take into account the database integrity constraints.
+     * @param sqlQueries SQL queries
+     * @return A list of INSERT statements allowing to test the SQL queries given in parameter
+     */
     public List<String> generateInsertListFor(String... sqlQueries) {
         List<SqlQuery> sqlQueryObjects = stream(sqlQueries)
                                         .map(SqlQuery::new)
